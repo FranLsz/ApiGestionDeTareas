754c8a5fde3450e20ca46531fe0eecebd95e4ca2
Fran <franlsz95develop@gmail.com>
2016-01-31 16:17:07 +0100

    Minor updates

--- ApiGestionDeTareas/App_Start/UnityConfig.cs
+++ ApiGestionDeTareas/App_Start/UnityConfig.cs
@@ -11,7 +11,7 @@ namespace ApiGestionDeTareas
     {
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
+            var container = new UnityContainer();
 
             // register all your components with the container here
             // it is NOT necessary to register your controllers
